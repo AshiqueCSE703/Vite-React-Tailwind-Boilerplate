bf0d5a395540db697df25f6fec54c435957f0c05
DebugCube-IT <debugcube@gmail.com>
2025-02-27 01:37:52 +0600

    Boilerplate-Upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="text-blue-500 text-center font-bold">
+      App.jsx as Homepage
+      </div>
+    </>
+  )
+}
+
+export default App
